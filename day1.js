f346ae86ea9e8f7f509f8b8287326d8cc7b0eef8
developerashikur1 <masik4506@gmail.com>
2023-04-08 14:43:35 +0600

    first commit

---
+++ day1.js
@@ -0,0 +1,12 @@
+
+  function sayHi() {
+      console.log(name);
+      console.log(age);
+      var name = 'Ladiya';
+      let age = 21;
+  }
+
+  sayHi();
+
+
+//   The ans will be first undefined, second ReferenceError
